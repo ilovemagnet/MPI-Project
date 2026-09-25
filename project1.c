54ad5b21d45ee3155f56eca110e1e384b375c031
Eugene Ablordey <eablordey@login2.cm.cluster>
2026-09-25 14:18:40 -0400

    Update MPI program

--- project1.c
+++ project1.c
@@ -10,14 +10,12 @@ int main(int argc, char *argv[]){
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     if (rank==0){
         printf("Hello world from processor %d of %d processors. \n", rank, size);
-        fflush(stdout);
         MPI_Send(&rank, 1, MPI_INT, rank+1, 0 , MPI_COMM_WORLD);
     }
     else{
         int number = 0;
         MPI_Recv(&number, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Hello world from processor %d of %d processors. \n", rank, size);
-        fflush(stdout);
         if(rank<size-1){
             MPI_Send(&rank, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
         }
